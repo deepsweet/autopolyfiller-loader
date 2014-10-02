808057edbd147290225252d457d17953c9026ef8
deepsweet <kir@soulshine.in>
2014-10-02 13:58:28 +0700

    cacheable

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ module.exports = function(source) {
     var polyfills = autopolyfiller.apply(autopolyfiller, browsers).add(source).polyfills;
     var inject = '';
 
+    if (this.cacheable) {
+        this.cacheable();
+    }
+
     // append require()s with absoluted paths to neccessary polyfills
     polyfills.forEach(function(polyfill) {
         inject += '\nrequire("' + require.resolve('polyfill/source/' + polyfill) + '");\n';
